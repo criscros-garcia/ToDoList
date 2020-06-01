603f8da5f1e6e7e5421bdcb6c0c0783c22288302
Cristian Garcia <eminemko@hotmail.com>
2020-06-01 13:27:22 -0500

    Remove an element when user clicks X

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -3,3 +3,12 @@
 $("li").click(function(){
 $(this).toggleClass("completed");
 });
+
+
+$("span").click(function(event){
+    $(this).toggleClass("deleted");
+    $(this).parent().fadeOut(500, function(){
+        $this.remove();
+    });
+    event.stopPropagation();
+});
